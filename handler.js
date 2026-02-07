f059849ab5770b14e94eff221a79013557cb2a75
Benyamin Ponce <benyaroot@gmail.com>
2026-02-07 16:02:44 -0500

    apppointment pe and cl

--- handler.js
+++ handler.js
@@ -1,30 +1,35 @@
 import { SNSClient, PublishCommand } from "@aws-sdk/client-sns";
 const snsClient = new SNSClient({});
 
-// LAMBDA A: Solo publica en SNS
 export const publicador = async (event) => {
-    const body = JSON.parse(event.body);
-    
-    await snsClient.send(new PublishCommand({
-        TopicArn: process.env.TOPIC_ARN,
-        Message: JSON.stringify(body),
-        Subject: "Nuevo Evento de Usuario"
-    }));
+    try {
+        const body = JSON.parse(event.body);
+        const { countryISO, nombre } = body;
 
-    return { 
-        statusCode: 200, 
-        body: JSON.stringify({ mensaje: "Evento publicado en SNS y distribuido a SQS" }) 
-    };
-};
+        const params = {
+            TopicArn: process.env.TOPIC_ARN,
+            Message: JSON.stringify(body),
+            Subject: "Nuevo Registro con Filtro",
+            // ESTO ACTIVA EL FILTRO EN SNS:
+            MessageAttributes: {
+                "countryISO": {
+                    DataType: "String",
+                    StringValue: countryISO.toLowerCase()
+                },
+                "nombre": {
+                    DataType: "String",
+                    StringValue: nombre
+                }
+            }
+        };
 
-// LAMBDA B: Procesa lo que llega de la cola
-export const worker = async (event) => {
-    for (const record of event.Records) {
-        // OJO: SNS mete su mensaje dentro del campo 'body' de SQS
-        const snsMessage = JSON.parse(record.body);
-        const userData = JSON.parse(snsMessage.Message);
-        
-        console.log("LOGISTICA: Procesando pedido para el usuario:", userData.nombre);
-        // Aquí podrías hacer un fetch a una API externa o guardar en DB
+        await snsClient.send(new PublishCommand(params));
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: `Estimado ${nombre}, Enviado con éxito para ${countryISO}` })
+        };
+    } catch (error) {
+        return { statusCode: 500, body: JSON.stringify({ error: error.message }) };
     }
 };
